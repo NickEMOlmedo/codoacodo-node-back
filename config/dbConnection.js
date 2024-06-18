7f51786d3d1e3e6822cc9dfbd477d5cd7bff1c2d
Nickolas Olmedo <86270160+NickEMOlmedo@users.noreply.github.com>
2024-06-17 22:20:09 -0300

    Actualizar dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,9 +1,9 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "viaduct.proxy.rlwy.net",
-  user: "root",
-  password: "oqlVOhWafxdRwJuuwabItmQgveiRbbmO",
-  database: "railway",
-  port: 46923,
+  host: " ",
+  user: " ",
+  password: " ",
+  database: " ",
+  port: ,
 });
